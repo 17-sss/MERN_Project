f5e20d4b9ad641dd3db3f2c88f9ebec4ac5b832c
17-sss <jhsonking@naver.com>
2020-09-14 22:18:13 +0900

    200914_2218

--- novshop/frontend_novshop/src/components/product/ProductDetailTemplate.js
+++ novshop/frontend_novshop/src/components/product/ProductDetailTemplate.js
@@ -1,141 +1,106 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
-import { getSize } from "../../lib/utility/customFunc";
-import { StyledHr, ClearEx } from "../common/StyleUtilModels";
+import styled, { css } from 'styled-components';
+import { getSize } from '../../lib/utility/customFunc';
+import { StyledHr, ClearEx } from '../common/StyleUtilModels';
 
-import productDataList from "../../lib/data/productList.json";
+import productDataList from '../../lib/data/productList.json';
 
 // 각종 함수 or 변수 START ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-const calcImageRatio = (AnNum, AstrType) => {      
-    const strType = (AstrType !== ("width" && "height") ? "err" : AstrType);
+const calcImageRatio = (AnNum, AstrType) => {
+    const strType = AstrType !== ('width' && 'height') ? 'err' : AstrType;
     const nDiv10 = Number(getSize(1.5, strType, false, true)) / 10;
-    return (nDiv10 * AnNum) + "px";
+    return nDiv10 * AnNum + 'px';
 };
 
 // 각종 함수 or 변수 END ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
-
-
-// 상품 상세 :: [공통] START ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 // ========================================================================================
-// ************* 상품 상세 -  Wrapper *************
+// [1] 상품 공통
 // ========================================================================================
+// 상품 상세 -  Wrapper
 const ProductDetailWrapper = styled.div`
     margin: 0 auto;
 
     ${(props) => {
-        const {mode} = props;        
-        const caseMode = (mode !== ("detail" && "additional") ? "detail" : mode);
-        
+        const { mode } = props;
+        const caseMode = mode !== ('detail' && 'additional') ? 'detail' : mode;
+
         switch (caseMode) {
-            case "detail":{
+            case 'detail': {
                 return css`
-                    width: ${getSize(1.5)};                
+                    width: ${getSize(1.5)};
                 `;
-            }           
-            case "additional": {
+            }
+            case 'additional': {
                 return css`
                     width: ${getSize(1.4)};
                 `;
             }
-        
-            default:    
+
+            default:
                 break;
         }
     }}
 `;
 // ---------------------------------------------------/
 
-
-// ========================================================================================
-// ************* 상품 상세 - 다용도 wrapper *************
-// ========================================================================================
+// 상품 상세 - 다용도 wrapper
 const ProductMultiWrapper = styled.div`
-    ${props => {
-        const {mode, height, width, isImg} = props;        
-        const caseMode = (mode !== ("detail" && "additional") ? "detail" : mode);        
+    ${(props) => {
+        const { mode, height, width } = props;
+        const caseMode = mode !== ('detail' && 'additional') ? 'detail' : mode;
 
-        switch (caseMode) {        
-            case "detail": {
+        switch (caseMode) {
+            case 'detail': {
                 return css`
-                    height: ${() => height || calcImageRatio(5, "width")};
-                    width: ${() => width || "50%"};  
-                    display: inline-block; 
-                    float:left;
-                    
-                    ${() => isImg 
-                    ? css`   
-                        div.imgWrapper {
-                            height: 80%;
-                            overflow: hidden;                    
-                            align-items: center;
-                            justify-content: center;      
-                            text-align: center;                  
-                            
-                            img {                            
-                                max-height: 100%;
-                                max-width: 100%;
-                                height: 100%;
-                                width: auto;                            
-                            }     
-                        }                                       
-                    `  
-                    : css`
-                        div.it_info {
-                            position: relative;                            
-                            padding: 0px 0 5px 0;
-                            text-align: left;
-                            border-bottom: 1px solid #e6e6e6;
-
-                            p.it_name {
-                                font-weight: bold;
-                                font-size: 14px;
-
-                                span.it_size {
-                                    font-size: 12px;
-                                    color: #ababab;
-                                    margin-left: 4px;
-                                    font-weight: normal;
-                                }
-                            }
-
-                            p.it_desc {
-                                padding: 5px 0;
-                                color: #ababab;
-                                font-size: 13px;
-                            }
-                        }
-                        
-                    `}                                
+                    height: ${() => height || calcImageRatio(5, 'width')};
+                    width: ${() => width || '50%'};
+                    display: inline-block;
+                    float: left;
                 `;
             }
-            case "additional": {
-                return css`
-                `;
-            }        
-            default:    
+            case 'additional': {
+                return css``;
+            }
+            default:
                 break;
         }
     }}
 `;
 // ---------------------------------------------------/
 
-// ========================================================================================
-// ************* 상품 상세 - DetailWrapper간의 여백 *************
-// ========================================================================================
+// 상품 상세 - DetailWrapper간의 여백
 const PaddingTB20 = styled.div`
     width: 100%;
     padding: 20px 0;
 `;
 // ---------------------------------------------------/
-// 상품 상세 :: [공통] END ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 
-
-
-// 상품 색상 :: [사각형] START ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 // ========================================================================================
-// ************* 상품 색상 - 상품 색상 정보 (사각형) Wrapper *************
+// [2] 상품 이미지
 // ========================================================================================
+// 상품 이미지 - Wrapper
+const ProductImageWrapper = styled.div`
+    height: 90%;
+    overflow: hidden;    
+                                
+    /* align-items: center;
+    justify-content: center;      
+    text-align: center; */
+       
+    img {
+        max-height: 100%;
+        max-width: 100%;
+        height: 100%;
+        width: auto;
+    }
+`;
+// ---------------------------------------------------/
+
+// ========================================================================================
+// [3] 상품 색상 정보
+// ========================================================================================
+// 상품 색상 정보 - 사각형 Wrapper
 const ProductSquareColorWrapper = styled.div`
     width: 100%;
     height: 20px;
@@ -143,10 +108,7 @@ const ProductSquareColorWrapper = styled.div`
 `;
 // ---------------------------------------------------/
 
-
-// ========================================================================================
-// ************* 상품 색상 - 상품 색상 정보 (사각형) Item *************
-// ========================================================================================
+// 상품 색상 정보 - 사각형 Item
 const ProductSquareColor = styled.span`
     width: 16px;
     height: 16px;
@@ -157,71 +119,100 @@ const ProductSquareColor = styled.span`
     font-size: 0;
     line-height: 0;
 
-    background-color: ${(props) => props.color ? props.color : "black"};
+    background-color: ${(props) => (props.color ? props.color : 'black')};
 `;
 // ---------------------------------------------------/
-// 상품 상세 :: [색상 - 사각형] END ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 
+// ========================================================================================
+// [4] 상품 일반 정보
+// ========================================================================================
+// 상품 일반 정보 - Wrapper
+const ProductInfo = styled.div`
+    position: relative;
+    padding: 0px 0 5px 0;
+    text-align: left;
+    border-bottom: 1px solid #e6e6e6;
+
+    p.it_name {
+        font-weight: bold;
+        font-size: 14px;
+
+        span.it_size {
+            font-size: 12px;
+            color: #ababab;
+            margin-left: 4px;
+            font-weight: normal;
+        }
+    }
+
+    p.it_desc {
+        padding: 5px 0;
+        color: #ababab;
+        font-size: 13px;
+    }
+`;
+// ---------------------------------------------------/
 
 const ProductDetailView = (props) => {
     // 추후 container 단 START ----------
-    const {itemId} = props.query;
+    const { itemId } = props.query;
 
-    const productData = // 추후에 백앤드로 findOne 하기. 
-        productDataList.find((item) => {        
-            return item.itemId === Number(itemId);        
-        });
-    
-    // eslint-disable-next-line
-    const {itemName, itemImage, itemSize, itemColors, price, sale, description} = productData;    
+    const productData = productDataList.find((item) => { // 추후에 백앤드로 findOne 하기.
+        return item.itemId === Number(itemId);
+    });
 
+    const {
+        itemName,
+        itemImage,
+        itemSize,
+        itemColors,
+        // eslint-disable-next-line
+        price,
+        // eslint-disable-next-line
+        sale,
+        description,
+    } = productData;
 
     // 추후 container 단 END -------
 
     return (
         <>
-            <PaddingTB20/>  
+            <PaddingTB20 />
             {/* 상품 상세 :: 구매, 이미지, 가격 등 */}
             <ProductDetailWrapper>
-                <ProductMultiWrapper isImg>
-                    <div className="imgWrapper">
-                        <img                                            
-                            src={itemImage}
-                            alt={itemName}
-                        />
-                    </div>
-                    <ProductSquareColorWrapper>
-                        {itemColors.map( (v, i) => {
-                            return (<ProductSquareColor color = {v} />);
-                        })}
-                        
-                    </ProductSquareColorWrapper>    
-                
+                <ProductMultiWrapper>
+                    <ProductImageWrapper>
+                        <img src={itemImage} alt={itemName} />
+                    </ProductImageWrapper>
                     
+                    <ProductSquareColorWrapper>
+                        {itemColors.map((v, i) => {
+                            return <ProductSquareColor key={i} color={v} />;
+                        })}
+                    </ProductSquareColorWrapper>
+
+               
                 </ProductMultiWrapper>
                 <ProductMultiWrapper>
-                    <div className = "it_info">
+                    <ProductInfo>
                         <p className="it_name">
-                            {itemName}                        
+                            {itemName}
                             <span className="it_size">
-                                {"[" + itemSize.join(", ") + "]"}
+                                {'[' + itemSize.join(', ') + ']'}
                             </span>
                         </p>
-                        <p className = "it_desc">
-                            {description}
-                        </p>
-                    </div>
+                        <p className="it_desc">{description}</p>
+                    </ProductInfo>
                 </ProductMultiWrapper>
 
-                <ClearEx/>
+                <ClearEx />
                 <PaddingTB20 />
-
             </ProductDetailWrapper>
-                        
-            <StyledHr/>
-            
+
+            <StyledHr />
+
             {/* 상품 상세 :: 추가정보(제품 상세정보) */}
-            <ProductDetailWrapper mode = {"additional"}>
+            <ProductDetailWrapper mode={'additional'}>
                 Test2
             </ProductDetailWrapper>
         </>
