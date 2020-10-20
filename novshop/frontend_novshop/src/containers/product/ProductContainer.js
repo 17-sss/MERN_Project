7943c98f873ae9293a5e64bb1dd5e0bc8d7c5f06
17-sss <jhsonking@naver.com>
2020-10-20 21:49:06 +0900

    201020_2149

--- novshop/frontend_novshop/src/containers/product/ProductContainer.js
+++ novshop/frontend_novshop/src/containers/product/ProductContainer.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import ProductTemplate from '../../components/product/ProductTemplate';
+import ProductForm from "../../components/product/ProductTemplate/ProductForm";
+import ProductItem from "../../components/product/ProductTemplate/ProductItem";
+import productData from "../../lib/data/productList.json";
 
-const ProductContainer = (props) => {
-    return (
-        <ProductTemplate {...props} />
+const ProductContainer = () => {
+    // [1] 여기에 데이터 조회하여 배열로 가져옴.
+    return (    
+        // /shopping/shirt/0?itemId=0
+        <ProductForm>
+            {productData.map( (v, i) => {
+                const {itemId, itemName, itemImage, itemSize, itemColors, price, sale, description, classification} = v;
+                const {mainclass, subclass} = classification;
+                const aLink = "/shopping/" + mainclass +"/"+ subclass+"?itemId="+itemId;
+
+                return (
+                    <ProductItem
+                        key = {itemId}
+                        itemName = {itemName} 
+                        itemImage = {itemImage}                                             
+                        itemLink = {aLink}
+                        itemSize = {itemSize}
+                        itemColors = {itemColors}
+                        price = {price}
+                        sale = {sale}
+                        description = {description}
+                    />
+                );
+            })}                        
+        </ProductForm>        
     );
 };
 
