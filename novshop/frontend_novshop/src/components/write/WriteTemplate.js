b1b734ca32cbf24ccfa1dea2faedfc3abafbbd29
17-sss <jhsonking@naver.com>
2020-11-17 17:43:35 +0900

    201117_1743

--- novshop/frontend_novshop/src/components/write/WriteTemplate.js
+++ novshop/frontend_novshop/src/components/write/WriteTemplate.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import styled/*, { css }*/ from 'styled-components';
 import { getSize } from '../../lib/utility/customFunc';
+import { cssCustomInput } from "../../components/common/StyleUtilCSS";
+
+import EditorContainer from '../../containers/write/EditorContainer';
 
 const WriteWrapper = styled.div`
     width: ${getSize(1.45)};
     margin: 0 auto;
 `;
 
+const WirteInput = styled.input`
+    ${cssCustomInput}
+`;
+
 const WriteTemplate = () => {
     return (
-        <WriteWrapper>WriteTemplate</WriteWrapper>
+        <WriteWrapper>
+            <WirteInput type="text" name="subject" placeholder="제목" />            
+            <EditorContainer />
+        </WriteWrapper>
     );
 };
 
