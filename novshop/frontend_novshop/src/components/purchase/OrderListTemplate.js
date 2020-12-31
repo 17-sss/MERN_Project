cbee146600b73d256695e44ff17d817a57bf82b1
17-sss <jhsonking@naver.com>
2020-12-31 13:14:00 +0900

    201231_1313

--- novshop/frontend_novshop/src/components/purchase/OrderListTemplate.js
+++ novshop/frontend_novshop/src/components/purchase/OrderListTemplate.js
@@ -8,10 +8,11 @@ const OrderListWrapper = styled.div`
 `;
 
 const OrderListMultiWrapper = styled.div`
+    width: 100%;
+
     ${(props) =>
         props.stype === 'pagename'
             ? css`
-                  width: 100%;
                   min-height: 30px;
                   margin: 50px 0 20px;
                   border-bottom: 0;
@@ -23,7 +24,44 @@ const OrderListMultiWrapper = styled.div`
                       font-size: 20px;
                   }
               `
-            : css``}
+            : props.stype === 'tableDiv'
+            ? css`
+                  border-top: 1px solid #e3e3e3;
+                  border-bottom: 1px solid #e3e3e3;
+                  margin: 20px auto;
+              `
+            : props.stype === 'headRow'
+            ? css`
+                  height: 30px;
+                  background-color: #fbfafa;
+              `
+            : props.stype === 'dataRow' &&
+              css`
+                  height: 30px;
+              `}
+`;
+
+const OrderListCell = styled.span`
+    display: inline-block;
+    width: ${(props) => (props.width ? props.width : '10%')};
+    vertical-align: center;
+
+    ${(props) =>
+        props.stype === 'head'
+            ? css`
+                  padding: 5px 0;
+                  margin: 0 0.5%;
+                  text-align: center;
+                  font-weight: bold;
+                  font-size: 15px;
+              `
+            : props.stype === 'data' &&
+              css`
+                  padding: 5px 0;
+                  margin: 0 5px;
+                  text-align: ${props.align ? props.align : 'center'};
+                  font-size: 13px;
+              `}
 `;
 
 const OrderListTemplate = () => {
@@ -33,9 +71,62 @@ const OrderListTemplate = () => {
                 <p>주문 내역</p>
             </OrderListMultiWrapper>
 
-            <OrderListMultiWrapper>
+            <OrderListMultiWrapper stype="tableDiv">
 
+                <OrderListMultiWrapper stype="headRow">
+                    <OrderListCell stype="head" width="12%">
+                        주문일자
+                    </OrderListCell>
+                    <OrderListCell stype="head" width="16%">
+                        이미지
+                    </OrderListCell>
+                    <OrderListCell stype="head" width="41%">
+                        상품정보
+                    </OrderListCell>
+                    <OrderListCell stype="head" width="12%">
+                        수량
+                    </OrderListCell>
+                    <OrderListCell stype="head" width="13%">
+                        상품구매금액
+                    </OrderListCell>
+                </OrderListMultiWrapper>
 
+                <OrderListMultiWrapper stype="dataRow">
+                    <OrderListCell stype="data" width="12%">
+                        20201231
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="16%">
+                        image
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="41%">
+                        info
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="12%">
+                        1
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="13%">
+                        20000
+                    </OrderListCell>
+                </OrderListMultiWrapper>
+
+                <OrderListMultiWrapper stype="dataRow">
+                    <OrderListCell stype="data" width="12%">
+                        20201231
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="16%">
+                        image
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="41%">
+                        info
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="12%">
+                        1
+                    </OrderListCell>
+                    <OrderListCell stype="data" width="13%">
+                        20000
+                    </OrderListCell>
+                </OrderListMultiWrapper>
+                
             </OrderListMultiWrapper>
         </OrderListWrapper>
     );
